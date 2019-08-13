8256f27896c07b3de78aaf910cebaf2f6d335fec
Zachary Kline <zkline888@gmail.com>
2019-08-13 19:26:04 -0400

    After four days of toil, finally done

--- nested.py
+++ nested.py
@@ -1,15 +1,54 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 """
-Module docstring: One line description of what your program does.
+Working code of the nested brackets assignment for Kenzie Academy Quarter 3
 """
-__author__ = "Your Github Username"
+__author__ = "ZacharyKline"
 
 import sys
 
-def main(args):
-    """Add your code here"""
-    
+openers = ['[', '(', '{', '<', '(*']
+closers = [']', ')', '}', '>', '*)']
+
+def arg_function(args):
+    """Goes through each character and checks it again a list of openers and closers."""
+    # results = []
+    stack = []
+    unbalanced = False
+    pos = 0
+    while args:
+        token = args[0]
+        if args[:2] == '(*' or args[:2] == '*)':
+            token = args[:2]
+        pos += 1
+
+        if token in closers:
+            index = closers.index(token)
+            match = openers[index]
+            if stack.pop() != match:
+                unbalanced = True
+                break #unbalanced
+
+        if token  in openers:
+            stack.append(token)
+        
+        args = args[len(token):]
+
+
+
+    if stack or unbalanced:
+        return 'NO' + str(pos)
+    return 'YES'
+
+
+def main():
+    with open('input.txt', 'r') as f:
+        with open('output.txt', 'w') as o:
+            for line in f:
+                read_output = arg_function(line)   
+                print(read_output)
+                o.write(read_output + '\n')
+
 
 if __name__ == '__main__':
-    main(sys.argv)
+    main()
